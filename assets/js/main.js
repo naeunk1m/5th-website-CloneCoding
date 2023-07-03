84343e7f7fbbc673635b4a3fcbfca1ba32872c91
naeunk1m <08enmik@gmail.com>
2023-07-03 21:27:19 +0900

    20230703

---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+$(function() {
+
+    gsap.to('.image-wrapper', {
+        scrollTrigger:{
+            trigger:"#image-scroll",
+            start:"0% 0%",
+            end:"100% 100%",
+            // markers:true,
+            scrub:0,
+            invalidateOnRefresh: true,
+        },
+        xPercent:-100,
+        x:function () { 
+            return window.innerWidth
+        }
+    })
+
+
+    $('.scroll-img-area .bg').each(function (index, element) {
+        console.log(element)
+        gsap.to($(this).find('img'), {
+            scrollTrigger:{
+                trigger:element,
+                start:"0% 100%",
+                end:"100% 0%",
+                markers:true,
+                scrub:1,
+            },
+            yPercent:-30,
+    
+        })
+    })
+
+    
+        gsap.to('#spread-scroll .bg', {
+            scrollTrigger:{
+                trigger:'#spread-scroll',
+                start:"50% 100%",
+                end:"100% 50%",
+                markers:true,
+                scrub:0,
+            },
+            width:0,
+        })
+
+
+
+        gsap.to('.sc-showcase2 .motion-img.img8 img', {
+            scrollTrigger:{
+                trigger:'.sc-showcase2 .motion-img.img8',
+                start:"0% 100%",
+                end:"100% 50%",
+                markers:true,
+                scrub:0,
+            },
+            width:'100vw',
+            height:'100vh',
+        })
+
+
+
+
+
+
+})
